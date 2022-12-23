5bcb6d7b28a9a2e6f019232f297d5a4b4ac15155
storycraft <storycraft@pancake.sh>
2022-12-24 01:40:05 +0900

    feat: initial database migration

---
+++ database/migrations/202212240119.sql
@@ -0,0 +1,47 @@
+/*
+ * Created on Sat Dec 24 2022
+ *
+ * Copyright (c) storycraft. Licensed under the GNU General Public License v3.
+ */
+
+CREATE TABLE account (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+
+    email VARCHAR(64) NOT NULL UNIQUE,
+    account_type VARCHAR(8) NOT NULL,
+    credential VARCHAR(128) NOT NULL,
+
+    created_at BIGINT NOT NULL,
+    activated_at BIGINT,
+    updated_at BIGINT NOT NULL,
+
+    suspended TINYINT NOT NULL
+);
+
+CREATE TABLE redirection (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+
+    account_id INT NOT NULL,
+
+    name VARCHAR(16) NOT NULL UNIQUE,
+    url VARCHAR(512) NOT NULL,
+
+    created_at BIGINT NOT NULL,
+    updated_at BIGINT NOT NULL,
+    
+    expire_at BIGINT,
+    visit_limit BIGINT,
+    
+    redirection_page TINYINT NOT NULL,
+    user_disabled TINYINT NOT NULL
+);
+
+CREATE TABLE visitlog (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+
+    redirection_id BIGINT NOT NULL,
+
+    visitor_address VARCHAR(64) NOT NULL,
+
+    visited_at BIGINT NOT NULL
+);
