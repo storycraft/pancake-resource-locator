7f07c9b5d0df4e2a68e1daf5d8e1a4a726316e9e
storycraft <storycraft@pancake.sh>
2022-12-24 18:19:22 +0900

    feat: add oauth controller

---
+++ api-server/src/main/java/sh/pancake/link/api/controller/OAuthController.java
@@ -0,0 +1,20 @@
+/*
+ * Created on Sat Dec 24 2022
+ *
+ * Copyright (c) storycraft. Licensed under the GNU General Public License v3.
+ */
+package sh.pancake.link.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("oauth")
+public class OAuthController {
+    @PostMapping("refresh")
+    public void refresh(@RequestParam("refresh_token") String refreshToken) {
+        
+    }
+}
