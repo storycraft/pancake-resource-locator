93c1b82c0365cab2faa7737d4f7a0d98fb662619
storycraft <storycraft@pancake.sh>
2022-12-25 01:22:19 +0900

    feat: add redirection api stub

--- api-server/src/main/java/sh/pancake/link/api/controller/RedirectController.java
+++ api-server/src/main/java/sh/pancake/link/api/controller/RedirectController.java
@@ -16,6 +16,7 @@ import sh.pancake.link.api.APIResult;
 import sh.pancake.link.api.redirect.RedirectStatusCode;
 import sh.pancake.link.api.service.RedirectService;
 import sh.pancake.link.repository.redirection.RedirectURL;
+import sh.pancake.link.repository.redirection.Redirection;
 
 /**
  * Redirect API controller
@@ -30,6 +31,11 @@ public class RedirectController {
     @Autowired
     private RedirectService service;
 
+    @GetMapping("{name}")
+    public APIResult<Redirection> redirection(@PathVariable("name") String name) {
+        // TODO:: implement stub
+        return APIResult.error(RedirectStatusCode.NOT_FOUND);
+    }
 
     @GetMapping("{name}/url")
     public APIResult<String> redirectURL(@PathVariable("name") String name) {
